032b960d706d5faf88b47785a714c5ad71190e7e
flynnA <flynnamos@hotmail.com>
2020-04-07 23:57:23 +0100

    jquery + optional html escaping

--- public/main.js
+++ public/main.js
@@ -3,22 +3,35 @@ const socket = io(); //Make a websocket connection to the server.
 socket.on('connect', () => {
     socket.on('tweetReceived', tweet => { //When the server posts a tweet,
         //show the tweet on the page.
-        document.getElementById('tweet.text').innerHTML = tweet.text; //OMG! UNESCAPED!!! who cares - twitter will be fixed soon
-        document.getElementById('tweet.author').innerHTML = tweet.author;
-        document.getElementById('tweet.date').innerHTML = new Date(tweet.date);
+        $('#tweet_text').html(tweet.text);
+        updatEscape();
+        $('#tweet_author').html(tweet.author);
+        $('#tweet_date').html(new Date(tweet.date));
     });
 });
 
 function sendTweet() {
-    let input = document.getElementById('new_tweet_text'); //
-    if (input.value.length == 0) return; //If the user hasn't input anything, stop here.
-    socket.emit('tweet', input.value); //Send the tweet text to the server.
-    input.value = ''; //Clear the input.
+    let input = $('#new_tweet_text').val();
+    if (input.length == 0) return; //If the user hasn't input anything, stop here.
+    socket.emit('tweet', input); //Send the tweet text to the server.
+    $('#new_tweet_text').val(''); //Clear the input.
     updateCharacterCounter(); //Reset the character counter back to zero.
 }
 
 function updateCharacterCounter() {
-    document.getElementById('character_count').innerHTML = document.getElementById('new_tweet_text').value.length; //Set the character counter to the amount of characters in the user's input.
+    $('#character_count').html($('#new_tweet_text').val().length); //Set the character counter to the amount of characters in the user's input.
+}
+
+function updatEscape() {
+    if ($('#escape_tweet').prop('checked')) { //If the user wants html to be escaped,
+        $('#tweet_text').html( //set the tweet's text to...
+            $('<div>').text($('#tweet_text').html()).html() //...its own text, but html escaped.
+        );
+    } else { //If the user doesn't want html to be escaped,
+        $('#tweet_text').html( //set the tweet's text to...
+            $('<div>').html($('#tweet_text').html()).text() //...its own text, but html unescaped.
+        );
+    }
 }
 
 function main() {
